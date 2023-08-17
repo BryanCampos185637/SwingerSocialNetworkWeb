ba72bda055acf9881db6236c5085d5b48443655a
Bryan Campos <bryanjose.185637@gmail.com>
2023-08-16 19:23:01 -0600

    avance en la maquetacion

--- SwingerSocialNetworkWeb/src/pages/Profile/ProfilePage.tsx
+++ SwingerSocialNetworkWeb/src/pages/Profile/ProfilePage.tsx
@@ -11,13 +11,13 @@ const ProfilePage = () => {
       <div className="row justify-content-center">
         <div className="col-sm-12 col-md-9 mt-3">
           <LazyLoadImage
-            className="border_radius"
+            className="border_radius shadow"
             src={getUrlWallpaperRandom()}
             width={"100%"}
             style={{ opacity: "0.8" }}
           />
         </div>
-        <div className="col-sm-12 col-md-9 d-flex justify-content-center content_img_profile">
+        <div className="col-sm-12 col-md-9 d-flex justify-content-center align-items-start content_img_profile">
           <div className="d-flex flex-column justify-content-center">
             <LazyLoadImage
               className="border_radius shadow"
@@ -25,6 +25,52 @@ const ProfilePage = () => {
               alt={`Foto de perfil ${displayName}`}
             />
             <p>{displayName}</p>
+          </div>
+          <div style={{ position: "absolute", marginTop: "25vh" }}>
+            <p className="my-description">
+              Soy un hombre felizmente casado, junto a mi esposa nos gusta
+              disfrutar de nuevas experiencias sexuales con otras parejas o
+              incluso tríos con chicas o chicos solteros.
+            </p>
+
+            <ul>
+              <li>
+                <a href="">Vive en Sonsonate</a>
+              </li>
+              <li>
+                <a href="">De Sonsonate, El Salvador</a>
+              </li>
+              <li>
+                <a href="">Miembre desde Marzo, 2023.</a>
+              </li>
+            </ul>
+            <div>
+              <ul className="nav nav-pills nav-fill">
+                <li className="nav-item">
+                  <button
+                    className="nav-link nav_select"
+                    aria-current="page"
+                  >
+                    Mi perfil
+                  </button>
+                </li>
+                <li className="nav-item">
+                  <button className="nav-link">
+                    Información personal
+                  </button>
+                </li>
+                <li className="nav-item">
+                  <button className="nav-link">
+                    Fotos
+                  </button>
+                </li>
+                <li className="nav-item">
+                  <button className="nav-link">
+                    Videos
+                  </button>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
